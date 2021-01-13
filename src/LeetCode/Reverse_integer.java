dfc1958d73af13751c8aab530f2de57f0f21bc86
Sally Oh <min27604@hotmail.com>
2021-01-13 11:58:42 +0900

    Algorithm-Leetcode

---
+++ src/LeetCode/Reverse_integer.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+// Given a signed 32-bit integer x, return x with its digits reversed.
+// If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+
+public class Reverse_integer {
+    public static int reverse(int x) {
+        long reversed = 0;
+        int sign = 1;
+
+        if (x < 0) {
+            sign *= -1;
+            x *= -1;
+        }
+
+        while (x > 0) {
+            reversed = reversed * 10 + (x % 10);
+            x /= 10;
+        }
+
+        if (sign == -1) {
+            reversed *= sign;
+        }
+
+        if ((reversed > Math.pow(-2, 31)) && (reversed < (Math.pow(2, 31) - 1))) {
+            return (int) reversed;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(2147483646));
+    }
+}
---
+++ src/LeetCode/Two_sum.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+public class Two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+}
