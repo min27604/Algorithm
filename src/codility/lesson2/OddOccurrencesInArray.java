94d7d7936748d2c1a475782b7b52558bfe1aef61
Sally Oh <min27604@hotmail.com>
2021-03-17 22:08:13 +0900

    Algorithm

--- src/codility/lesson2/OddOccurrencesInArray.java
+++ src/codility/lesson2/OddOccurrencesInArray.java
@@ -1,16 +1,22 @@
 package codility.lesson2;
 
+import java.util.Arrays;
+
 public class OddOccurrencesInArray {
-    public int solution(int[] A) {
+    public static int solution(int[] A) {
         for (int i = 0; i < A.length; i++) {
             if (A[i] == 0) {
                 continue;
             }
 
-            for (int j = i; j < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (j == i) {
+                    continue;
+                }
                 if (A[j] == A[i]) {
-                    A[i] = 0;
                     A[j] = 0;
+                    A[i] = 0;
+                    break;
                 }
             }
         }
@@ -21,5 +27,25 @@ public class OddOccurrencesInArray {
             }
         }
         return 0;
+    }
+
+    public static int solution2(int[] A) {
+        Arrays.sort(A);
+
+        for (int i = 0; i < A.length; i += 2) {
+            if (i == A.length - 1) {
+                return A[i];
+            }
+
+            if (A[i] != A[i + 1]) {
+                return A[i];
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 3, 9, 3, 9, 7, 9};
+        System.out.println(solution2(arr));
     }
 }
