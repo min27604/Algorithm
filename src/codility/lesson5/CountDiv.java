e18cf2a7cf2b31e531f2b38440dae478611fec09
Sally Oh <min27604@hotmail.com>
2021-04-12 23:45:10 +0900

    Algorithm

---
+++ src/codility/lesson5/CountDiv.java
@@ -0,0 +1,10 @@
+package codility.lesson5;
+
+public class CountDiv {
+    public int solution(int A, int B, int K) {
+        if (A % K == 0) {
+            return B / K - A / K + 1;
+        }
+        return B / K - A / K;
+    }
+}
